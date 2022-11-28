3421784b6d2c8fed1f6823a2dfc5a64f1c304d88
ShivaniPatid <shivanipatidar712@gmail.com>
2022-11-28 16:08:00 +0530

    add routing

--- FundooNotes/src/app/app-routing.module.ts
+++ FundooNotes/src/app/app-routing.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ForgetPasswordComponent } from './components/forget-password/forget-password.component';
 import { LoginComponent } from './components/login/login.component';
 import { RegistrationComponent } from './components/registration/registration.component';
+import { ResetPasswordComponent } from './components/reset-password/reset-password.component';
 
 const routes: Routes = [
   { path:'register', component: RegistrationComponent},
-  { path:'login', component: LoginComponent}
+  { path:'login', component: LoginComponent},
+  { path:'forget', component: ForgetPasswordComponent},
+  { path:'reset', component: ResetPasswordComponent}
 ];
 
 @NgModule({
