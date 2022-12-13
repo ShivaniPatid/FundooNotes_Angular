b71f46d7af6c968cf83afe8cfbae48ddb45d6291
ShivaniPatid <shivanipatidar712@gmail.com>
2022-12-13 19:45:10 +0530

    api integration for archive note

---
+++ FundooNotes/src/app/components/archive-note/archive-note.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit  } from '@angular/core';
+import { NoteService } from 'src/app/services/noteServices/note.service';
+
+@Component({
+  selector: 'app-archive-note',
+  templateUrl: './archive-note.component.html',
+  styleUrls: ['./archive-note.component.scss']
+})
+export class ArchiveNoteComponent implements OnInit{
+
+  noteArray=[]
+  constructor(private note : NoteService) { }
+
+  ngOnInit(): void {
+    this.getArchiveNote()
+  }
+
+  getArchiveNote(){
+    this.note.getAllNoteService().subscribe((response : any) => {
+      console.log(response);
+      this.noteArray=response
+      console.log(this.noteArray);
+      this.noteArray=this.noteArray.filter((result:any)=>{
+        return result.isTrash==false;
+      })
+    })
+  }
+}
